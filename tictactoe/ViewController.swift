8f25d6cb4c8038e4c48ac3d6bbec7b9bbdd9b25e
kilbergr <rekilberg@gmail.com>
2016-01-08 12:03:50 -0800

    made some slight changes

--- tictactoe/ViewController.swift
+++ tictactoe/ViewController.swift
@@ -37,7 +37,7 @@ class ViewController: UIViewController {
         newGameButton.setTitle(" ", forState: UIControlState.Normal)
         
         var buttonToClear : UIButton
-        for i in 0...8 {
+        for i in 0..<9 {
             buttonToClear = view.viewWithTag(i) as! UIButton
             buttonToClear.setImage(nil, forState: .Normal)
             
@@ -58,7 +58,7 @@ class ViewController: UIViewController {
                     //keep track of state of each square
                     gameStateArr[sender.tag] = 1
                     
-                    xOdd = 2    
+                    xOdd = 2
                     xoCount++
                 }
                     
@@ -72,12 +72,6 @@ class ViewController: UIViewController {
                     xOdd = 1
                     xoCount++
 
-                }
-                if xoCount == 9 {
-                    onWin.text = "It's a Draw!"
-                    offerNew()
-                    showWinner()
-                    
                 }
                 
                 //to check for winning combinations
@@ -98,7 +92,18 @@ class ViewController: UIViewController {
                             offerNew()
                         }
                     }
-                }
+                    
+                    //if there's a draw
+                    else {
+                        if xoCount > 8 {
+                            print(gameStateArr[combo[0]], gameStateArr[combo[1]], gameStateArr[combo[2]])
+                            onWin.text = "It's a Draw!"
+                            offerNew()
+                            showWinner()
+                            
+                        }
+                    }
+            }
         }
     }
     
